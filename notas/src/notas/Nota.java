5a82e409bbdcc182f0514839d76c702f7a0f5e4e
Victor Miguez <v.miguez2004@gmail.com>
2023-05-18 21:49:57 -0300

    alunoNotas

--- notas/src/notas/Nota.java
+++ notas/src/notas/Nota.java
@@ -1,5 +1,22 @@
 package notas;
 
 public class Nota {
-
+	private double valor;
+	private String data;
+	
+	
+	public double getValue() {
+		return valor;
+	}
+	public void setValue(double value) {
+		this.valor = value;
+	}
+	public String getDate() {
+		return data;
+	}
+	public void setDate(String date) {
+		this.data = date;
+	}
+	
+	
 }
--- notas/src/notas/Pessoa.java
+++ notas/src/notas/Pessoa.java
@@ -1,5 +1,26 @@
 package notas;
 
 public class Pessoa {
-
+	protected String nome;
+	protected String cpf;
+	
+	public Pessoa(String nome, String cpf) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+	
+	public String getName() {
+		return nome;
+	}
+	public void setName(String name) {
+		this.nome = name;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
 }
