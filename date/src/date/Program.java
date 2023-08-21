e337b5d0396d08700aa2cd737bd001ab6da2978b
Victor Miguez <dev.miguezz@gmail.com>
2023-08-21 17:37:05 -0300

    feature: help victor to study dates in Java

--- date/src/date/Program.java
+++ date/src/date/Program.java
@@ -3,6 +3,7 @@ package date;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class Program {
@@ -18,7 +19,7 @@ public class Program {
 		
 		LocalDate d01 = LocalDate.parse("2004-05-12"); //format ISO-8601 to LocalDate
 		LocalDateTime t01 = LocalDateTime.parse("2004-05-12T13:21:12.2"); //format ISO-8601 to LocalDateTime
-		Instant i01 = Instant.parse("2028-06-21T00:00:02Z"); // date + GMT hour (Z)
+		Instant i01 = Instant.parse("2028-06-21T00:00:02Z"); //date + GMT hour (Z)
 		Instant iPegadinha = Instant.parse("2023-08-15T10:10:10-03:00"); //hora no fuso de brasilia (-03:00)
 		
 		LocalDate d02 = LocalDate.parse("12/05/2004", fmt1); //passamos o formato definido anteriormente para utilizarmos este modelo. eh como uma sobrecarga
@@ -30,14 +31,35 @@ public class Program {
 		System.out.println(t);
 		System.out.println(i);
 		
+		System.out.println("-----------------------");
+
 		System.out.println(d01);
 		System.out.println(t01);
 		System.out.println(i01);
 		System.out.println(iPegadinha); //imprime no GMT
 		
+		System.out.println("-----------------------");
+
 		System.out.println(d02);
 		System.out.println(d03);
 		System.out.println(d04);
+		
+		System.out.println("-----------------------");
+		
+		DateTimeFormatter formatoDia = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		LocalDate ontem = LocalDate.parse("2023-08-20");
+		System.out.println("Ontem: " + ontem.format(formatoDia));
+		
+		LocalDate hoje = LocalDate.now();
+		System.out.println("Hoje: " + hoje.format(formatoDia));
+		
+		DateTimeFormatter formatoDiaHora = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		LocalDateTime agora = LocalDateTime.now();
+		System.out.println("Agora: " + agora.format(formatoDiaHora));
+		
+		DateTimeFormatter instanteEspecial = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault()); //timezone do pc
+		Instant algumDia = Instant.parse("2023-07-20T01:30:26Z");
+		System.out.println("Dia: " + instanteEspecial.format(algumDia));
 	}
 
 }
